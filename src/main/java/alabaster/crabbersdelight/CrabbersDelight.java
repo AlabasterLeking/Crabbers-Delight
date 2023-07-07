61a6cd614af0d5362e73ac32d8a8cc6569282c4f
cjtop <cjtopher2012@gmail.com>
2023-07-07 09:23:00 -0400

    Setup of mod

--- src/main/java/alabaster/crabbersdelight/CrabbersDelight.java
+++ src/main/java/alabaster/crabbersdelight/CrabbersDelight.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package alabaster.crabbersdelight;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
@@ -13,12 +13,9 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -27,8 +24,8 @@ import net.minecraftforge.registries.RegistryObject;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(CrabbersDelight.MODID)
+public class CrabbersDelight
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "examplemod";
@@ -44,7 +41,7 @@ public class ExampleMod
     // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
     public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("example_block", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS)));
 
-    public ExampleMod()
+    public CrabbersDelight()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
