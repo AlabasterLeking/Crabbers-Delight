0869bb0a490d2d7099c88fc0fa034001753ba49f
cjtop <cjtopher2012@gmail.com>
2023-07-12 08:35:55 -0400

    Crab Barrels

--- src/main/java/alabaster/crabbersdelight/init/ModBlocks.java
+++ src/main/java/alabaster/crabbersdelight/init/ModBlocks.java
@@ -2,9 +2,17 @@ package alabaster.crabbersdelight.init;
 
 import alabaster.crabbersdelight.CrabbersDelight;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import static alabaster.crabbersdelight.CrabbersDelight.BLOCKS;
 
 public class ModBlocks {
-    public static final DeferredRegister<Block> ITEMS = DeferredRegister.create(ForgeRegistries.BLOCKS, CrabbersDelight.MODID);
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, CrabbersDelight.MODID);
+
+    public static final RegistryObject<Block> CRAB_BARREL = BLOCKS.register("crab_barrel.json",
+            () -> new Block(Block.Properties.of(Material.WOOD).strength(2.0F, 3.0F).sound(SoundType.WOOD)));
 }
