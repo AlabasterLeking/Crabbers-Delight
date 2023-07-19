eb1de94920c3a167a0dc86c8afe4ebeec2d0865a
cjtop <cjtopher2012@gmail.com>
2023-07-18 21:20:46 -0400

    Attempting to get blocks working - creates model but has no textures currently

--- src/main/java/alabaster/crabbersdelight/common/registry/ModBlocks.java
+++ src/main/java/alabaster/crabbersdelight/common/registry/ModBlocks.java
@@ -1,6 +1,5 @@
 package alabaster.crabbersdelight.common.registry;
 
-import alabaster.crabbersdelight.CrabbersDelight;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Items;
@@ -10,11 +9,13 @@ import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Material;
+
+import net.minecraft.world.level.material.PushReaction;
+import net.minecraftforge.registries.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import vectorwing.farmersdelight.FarmersDelight;
+import alabaster.crabbersdelight.CrabbersDelight;
 import vectorwing.farmersdelight.common.block.*;
 
 import java.util.function.ToIntFunction;
@@ -26,7 +27,7 @@ public class ModBlocks {
         return (state) -> state.getValue(BlockStateProperties.LIT) ? lightValue : 0;
     }
 
-    public static final RegistryObject<Block> CRAB_BARREL = BLOCKS.register("crab_barrel.json",
-            () -> new Block(Block.Properties.of(Material.WOOD).strength(2.0F, 3.0F).sound(SoundType.WOOD)));
-    }
-
+    // Food Storage
+    public static final RegistryObject<Block> CRAB_BARREL = BLOCKS.register("crab_barrel",
+            () -> new Block(Block.Properties.copy(Blocks.OAK_PLANKS).strength(2.0F, 3.0F).sound(SoundType.WOOD)));
+}
--- src/main/java/alabaster/crabbersdelight/common/registry/ModItems.java
+++ src/main/java/alabaster/crabbersdelight/common/registry/ModItems.java
@@ -1,15 +1,30 @@
 package alabaster.crabbersdelight.common.registry;
 
 import alabaster.crabbersdelight.CrabbersDelight;
+import com.google.common.collect.Sets;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
-import vectorwing.farmersdelight.common.FoodValues;
+import alabaster.crabbersdelight.common.registry.CDFoodValues;
+import alabaster.crabbersdelight.CrabbersDelight;
+
+import java.util.LinkedHashSet;
+import java.util.function.Supplier;
 
 public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CrabbersDelight.MODID);
+    public static LinkedHashSet<RegistryObject<Item>> CREATIVE_TAB_ITEMS = Sets.newLinkedHashSet();
+
+    public static RegistryObject<Item> registerWithTab(final String name, final Supplier<Item> supplier) {
+        RegistryObject<Item> block = ITEMS.register(name, supplier);
+        CREATIVE_TAB_ITEMS.add(block);
+        return block;
+    }
 
     // Basic Items
     public static Item.Properties basicItem() {
@@ -21,7 +36,7 @@ public class ModItems {
         return new Item.Properties().food(food).tab(CrabbersDelight.CREATIVE_TAB);
     }
 
-    // Begin Item Registry
+    // Food
     public static final RegistryObject<Item> RAW_CRAB = ITEMS.register("raw_crab",
             () -> new Item(foodItem(CDFoodValues.RAW_CRAB).tab(CrabbersDelight.CREATIVE_TAB)));
 
@@ -33,4 +48,8 @@ public class ModItems {
 
     public static final RegistryObject<Item> COOKED_SHRIMP= ITEMS.register("cooked_shrimp",
             () -> new Item(foodItem(CDFoodValues.COOKED_SHRIMP).tab(CrabbersDelight.CREATIVE_TAB)));
+
+    // Blocks
+    public static final RegistryObject<Item> CRAB_BARREL = registerWithTab("crab_barrel",
+            () -> new BlockItem(ModBlocks.CRAB_BARREL.get(), basicItem()));
     }
