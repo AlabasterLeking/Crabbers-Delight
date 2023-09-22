14e905a6f6ab499af721ac5c19f6e885d0dfdc8e
Christopher Walker <cjtopher2012@gmail.com>
2023-09-21 22:20:40 -0400

    Fixed crab trap - iterated release

--- src/main/java/alabaster/crabbersdelight/common/event/CrabTrap.java
+++ src/main/java/alabaster/crabbersdelight/common/event/CrabTrap.java
@@ -2,11 +2,15 @@ package alabaster.crabbersdelight.common.event;
 
 import alabaster.crabbersdelight.CrabbersDelight;
 import alabaster.crabbersdelight.common.block.container.CrabTrapMenu;
+import alabaster.crabbersdelight.common.registry.ModBlocks;
+import net.minecraft.client.renderer.ItemBlockRenderTypes;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(modid = CrabbersDelight.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CrabTrap {
@@ -16,5 +20,10 @@ public class CrabTrap {
         if (event.getAtlas().location().equals(TextureAtlas.LOCATION_BLOCKS)) {
             event.addSprite(CrabTrapMenu.BAIT_SLOT);
         }
+    }
+
+    @SubscribeEvent
+    public static void clientSetup(FMLClientSetupEvent event) {
+        ItemBlockRenderTypes.setRenderLayer(ModBlocks.CRAB_TRAP.get(), RenderType.cutout());
     }
 }
