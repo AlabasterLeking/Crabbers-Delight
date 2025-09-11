4e0321db57d6a2dce535cccc86498e0bf3ffdacd
AlabasterLeking <cjtopher2012@gmail.com>
2025-09-10 22:13:57 -0400

    Fixed loot table

--- src/main/java/alabaster/crabbersdelight/data/loot/CDBlockLoot.java
+++ src/main/java/alabaster/crabbersdelight/data/loot/CDBlockLoot.java
@@ -32,6 +32,7 @@ public class CDBlockLoot extends BlockLootSubProvider
         dropSelf(CDModBlocks.GLOW_SQUID_BARREL.get());
         dropSelf(CDModBlocks.FROG_LEG_BARREL.get());
         dropSelf(CDModBlocks.SEA_PICKLE_CRATE.get());
+        dropSelf(CDModBlocks.SCUTE_BLOCK.get());
         dropSelf(CDModBlocks.NAUTILUS_SHELL_BLOCK.get());
         dropSelf(CDModBlocks.PEARL_BLOCK.get());
     }
